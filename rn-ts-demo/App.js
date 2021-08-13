f38bdd70df52f3976df2c779e15863115c16dcec
Simge Atlıhan <73952475+simgeatlhn@users.noreply.github.com>
2021-08-13 11:12:06 +0300

    rn-ts-demo

---
+++ rn-ts-demo/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './components/App';
+
+export default class AppRoot extends React.Component {
+  render() {
+    return <App />;
+  }
+}
+
+
---
+++ rn-ts-demo/components/App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Hello from "./Hello";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container} >
+                <Text>Open up App.js to start working on your app!</Text>
+                <Hello name="simge" enthusiasmLevel={1} />
+                <StatusBar style="auto" />
+            </View>
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
